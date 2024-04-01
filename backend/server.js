ea6b6f9d12e24ade54b11264dbd14ace7379b3bd
Jonas Leite <jonas.programador01@gmail.com>
2024-04-01 13:53:26 -0300

    feat: impleting a static link for each image in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-import  express  from "express";
+import express from "express";
 import { testConnection } from "./database/connection.js";
 
 
 const app = express()
 const port = 7777
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
+
+app.use('/', routes);
+app.use(express.static('public'));
 
 app.use(express.json())
 
-app.listen(port,()=> testConnection(), console.log(`Server is running on the port  ${port} `))
+app.listen(port, () => testConnection(), console.log(`Server is running on the port  ${port} `))
