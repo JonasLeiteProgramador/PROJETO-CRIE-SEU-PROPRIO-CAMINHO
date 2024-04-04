d3f0a150a23323ae3082cb3479764e4bc9663cbf
Jonas Leite <jonas.programador01@gmail.com>
2024-04-04 13:23:28 -0300

    style: fixing the responsive desing for the cellphone

--- frontend/src/screen/projects/projects.jsx
+++ frontend/src/screen/projects/projects.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './projects.css';
+import '../../root.css'
 import Header from '../../components/header/header';
 
 const ProjectList = () => {
